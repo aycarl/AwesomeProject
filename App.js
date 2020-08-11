26ec24f64eada42992500d57893b241831ed5d7c
Carl <carl.agbenyega@gmail.com>
2020-08-10 23:12:52 -0600

    first react native app

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [displayText, changeDisplayText] = useState("Open up App.js to start working on your app!")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{displayText}</Text>
+      <Button title="Change Text" onPress={() => changeDisplayText("Something new. Yaay!")}/>
       <StatusBar style="auto" />
     </View>
   );
